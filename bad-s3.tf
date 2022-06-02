e7ff85536c9a87c484f22bfdabd8f1a12e2fdf40
davesc63 <66445532+davesc63@users.noreply.github.com>
2022-06-02 15:03:08 +1000

    Update bad-s3.tf
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- bad-s3.tf
+++ bad-s3.tf
@@ -8,6 +8,19 @@ resource "aws_s3_bucket" "data" {
   force_destroy = true
 }
 
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "data" {
+  bucket = aws_s3_bucket.data.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+
+
 resource "aws_s3_bucket" "financials" {
   # bucket is not encrypted
   # bucket does not have access logs
