10414a852f14a8f5687d20985051d6ae564ac5ae
davesc63 <66445532+davesc63@users.noreply.github.com>
2022-06-02 13:49:24 +1000

    iac demo

---
+++ bad-s3.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "private"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "financials" {
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-financials"
+  acl           = "private"
+  force_destroy = true
+}
