2d8e49e2f92eda96c16ac6f3a85aadbab9e16003
davesc63 <66445532+davesc63@users.noreply.github.com>
2022-06-04 14:31:36 +1000

    gitops demo

--- bad-iac.tf
+++ bad-iac.tf
@@ -3,14 +3,14 @@ resource "aws_s3_bucket" "data" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
-  bucket        = "bad-s3-bucket-data"
+  bucket        = "gitops-demo-s3-bucket-data"
   acl           = "private"
   force_destroy = true
 }
 
 
 resource "aws_s3_bucket" "data_log_bucket" {
-  bucket = "bad-s3-data-log-bucket"
+  bucket = "gitops-demo-s3-data-log-bucket"
 }
 
 resource "aws_s3_bucket_logging" "data" {
