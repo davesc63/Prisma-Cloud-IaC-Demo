9b96243e60ddd20b8bb78c7f3ea5cd52953ff667
davesc63 <66445532+davesc63@users.noreply.github.com>
2022-06-05 13:50:34 +1000

    gitops demo updated

--- gitops-demo.tf
+++ gitops-demo.tf
@@ -3,14 +3,14 @@ resource "aws_s3_bucket" "data" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
-  bucket        = "gitops-demo-s3-bucket-data"
+  bucket        = "gitops-demo-s3-bucket-data-updated-v2"
   acl           = "private"
   force_destroy = true
 }
 
 
 resource "aws_s3_bucket" "data_log_bucket" {
-  bucket = "gitops-is-awseome"
+  bucket = "gitops-is-awseome-updated-v2"
 }
 
 resource "aws_s3_bucket_logging" "data" {
