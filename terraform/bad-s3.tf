d33a7958d2e63651b61b0d1a431ff4ee7ae07084
davesc63 <66445532+davesc63@users.noreply.github.com>
2022-07-29 09:43:21 +1000

    Update terraform/bad-s3.tf
    Co-authored-by: prisma-cloud-devsecops[bot] <89982750+prisma-cloud-devsecops[bot]@users.noreply.github.com>

--- terraform/bad-s3.tf
+++ terraform/bad-s3.tf
@@ -14,3 +14,13 @@ resource "aws_s3_bucket" "data" {
     git_repo = "Prisma-Cloud-IaC-Demo-update"
   })
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "data" {
+  bucket = aws_s3_bucket.data.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
