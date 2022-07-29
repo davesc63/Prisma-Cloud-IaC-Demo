8e053a44f5615d99ea9cc0d066d8f01fbac0ee24
davesc63 <66445532+davesc63@users.noreply.github.com>
2022-07-29 11:10:07 +1000

    bads3 reset

--- terraform/bad-s3.tf
+++ terraform/bad-s3.tf
@@ -15,13 +15,3 @@ resource "aws_s3_bucket" "data" {
     customer = "Customer-Name-Here"
   })
 }
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "data" {
-  bucket = aws_s3_bucket.data.bucket
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
-    }
-  }
-}
