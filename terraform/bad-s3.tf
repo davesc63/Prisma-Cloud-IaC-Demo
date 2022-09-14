7df8c07879d5511b78df4d34d933c3fb9868cfcc
davesc63 <66445532+davesc63@users.noreply.github.com>
2022-09-14 12:14:57 +1000

    Update terraform/bad-s3.tf
    Co-authored-by: prisma-cloud-devsecops[bot] <89982750+prisma-cloud-devsecops[bot]@users.noreply.github.com>

--- terraform/bad-s3.tf
+++ terraform/bad-s3.tf
@@ -19,6 +19,20 @@ resource "aws_s3_bucket" "data" {
 }
 
 
+resource "aws_s3_bucket" "data_log_bucket" {
+  bucket = "data-log-bucket"
+}
+
+resource "aws_s3_bucket_logging" "data" {
+  bucket = aws_s3_bucket.data.id
+
+  target_bucket = aws_s3_bucket.data_log_bucket.id
+  target_prefix = "log/"
+}
+
+
+
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "data" {
   bucket = aws_s3_bucket.data.bucket
 
