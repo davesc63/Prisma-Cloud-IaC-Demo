4e26a44b42cb63d13fa4397f7bc3fa60a83b7b3c
davesc63 <66445532+davesc63@users.noreply.github.com>
2022-09-14 12:08:07 +1000

    ccx demo

--- terraform/bad-s3.tf
+++ terraform/bad-s3.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "data" {
   # bucket is not encrypted
   # bucket does not have access logs
   # bucket does not have versioning
-  bucket        = "${local.resource_prefix.value}-data"
+  bucket        = "${local.resource_prefix.value}-data-ccx-demo"
   acl           = "private"
   force_destroy = true
   tags = merge({
@@ -12,8 +12,19 @@ resource "aws_s3_bucket" "data" {
     }, {
     git_org  = "davesc63"
     git_repo = "Prisma-Cloud-IaC-Demo-update"
-    customer = "Customer-Name-Here"
+    customer = "CCX-Demo"
     }, {
     yor_trace = "28cabed6-3d82-40c6-b2be-7323c0a7c8d5"
   })
+}
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "data" {
+  bucket = aws_s3_bucket.data.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
