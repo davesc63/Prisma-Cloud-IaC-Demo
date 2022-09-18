d55092026e34fcaa9d1eeed14458c1c57258762d
davesc63 <66445532+davesc63@users.noreply.github.com>
2022-09-19 09:17:53 +1000

    repo setup

---
+++ terraform/bad-s3.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "private"
+  force_destroy = true
+  tags = merge({
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+    }, {
+    git_org  = "davesc63"
+    git_repo = "Prisma-Cloud-IaC-Demo-update"
+    customer = "Customer-Name-Here"
+    }, {
+    yor_trace = "28cabed6-3d82-40c6-b2be-7323c0a7c8d5"
+  })
+}
