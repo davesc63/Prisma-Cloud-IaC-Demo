11eb5cab21de640f4742327884d9a9d409413d68
davesc63 <66445532+davesc63@users.noreply.github.com>
2022-11-02 06:05:22 +1000

    ecs-image.tf

---
+++ terraform/ecs-image.tf
@@ -0,0 +1,18 @@
+resource "aws_ecs_task_definition" "service" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "ubuntu:jammy-20221020"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
