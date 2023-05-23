ed6e82125509ca8957dca8e01a758214bbbd081d
davesc63 <66445532+davesc63@users.noreply.github.com>
2023-05-23 11:13:01 +1000

    repo setup

---
+++ terraform/perth-s3.tf
@@ -0,0 +1,42 @@
+# resource "aws_s3_bucket" "perthdemo" {
+#   # bucket is public
+#   # bucket is not encrypted
+#   # bucket does not have access logs
+#   # bucket does not have versioning
+#   # backup
+#   bucket        = "${local.resource_prefix.value}-perth-bucket"
+#   acl           = "public-read"
+#   force_destroy = false
+#   tags = merge({
+#     Name        = "${local.resource_prefix.value}-perth-demo"
+#     Environment = local.resource_prefix.value
+#     }, {
+#     git_org  = "davesc63"
+#     git_repo = "Prisma-Cloud-IaC-Demo"
+#     customer = "Perth-IaC-Demo"
+#     }, {
+#     yor_trace = ""
+#   })
+# }
+resource "aws_s3_bucket" "perthdemo" {
+  bucket = "${local.resource_prefix.value}-perth-demo"
+  tags = merge({
+    Name        = "${local.resource_prefix.value}-perth-demo"
+    Environment = local.resource_prefix.value
+    }, {
+    git_org  = "davesc63"
+    git_repo = "Prisma-Cloud-IaC-Demo"
+    customer = "Perth-IaC-Demo"
+    }, {
+    yor_trace = ""
+  })
+}
+
+resource "aws_s3_bucket_public_access_block" "perthdemo" {
+  bucket = aws_s3_bucket.perthdemo.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
