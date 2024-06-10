72b55ac64fad14124e17c113a6b8af70fd0dfa63
David McGovern <dmcgovern@m-qp7rm69k5f.paloaltonetworks.local>
2024-06-11 09:06:55 +1000

    repo setup

---
+++ terraform/perth-s3.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "perthdemo" {
+  # backup commit
+  bucket = "${local.resource_prefix.value}-perth-demo"
+  tags = merge({
+    Name        = "${local.resource_prefix.value}-perth-demo"
+    Environment = local.resource_prefix.value
+    }, {
+    git_org  = "davesc63"
+    git_repo = "Prisma-Cloud-IaC-Demo1"
+    customer = "Perth-IaC-Demo"
+    }, {
+    yor_trace = "522dc9a4-AAAA-BBBB-CCCC-7b94b4c5d3bf"
+  })
+}
+
